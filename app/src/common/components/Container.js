8eb4e5a39cd89341ca11a251f84efdd8be8add9f
mi-mazouz <mickaelmazouz@gmail.com>
2018-07-14 23:26:07 +0200

    sign-in view

---
+++ app/src/common/components/Container.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const StyledContainer = styled.div`
+  text-align: center;
+`
+
+const Container = ({ children }) => (
+  <StyledContainer className="container">
+    { children }
+  </StyledContainer>
+)
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+export default Container
--- app/src/config/font-awesome.js
+++ app/src/config/font-awesome.js
@@ -1,16 +1,26 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {
+  faCheck,
+  faEnvelope,
+  faExclamationTriangle,
   faGenderless,
+  faLock,
   faMapMarkerAlt,
   faMars,
   faTransgender,
+  faUser,
   faVenus
 } from '@fortawesome/free-solid-svg-icons'
 
 library.add(
+  faCheck,
+  faEnvelope,
+  faExclamationTriangle,
   faGenderless,
+  faLock,
   faMapMarkerAlt,
   faMars,
   faTransgender,
+  faUser,
   faVenus
 )
