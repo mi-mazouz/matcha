3e6801a8618b9e762c51948175b1690d48499b1d
joris carol <joriscarol@MacBook-Pro-de-joris.local>
2018-07-17 13:36:06 +0200

    Fixed conflicts "Hello World" 2

--- app/src/Layouts/LogoutLayout.jsx
+++ app/src/Layouts/LogoutLayout.jsx
@@ -15,23 +15,22 @@ import landingPageBackgroundImage from '../pages/landing/assets/background.jpg'
 const Page = styled(StyledPage)`
   background-image: url(${landingPageBackgroundImage2x});
   @media screen and (max-width : 568px) {
-   background-image: url(${landingPageBackgroundImage});
+    background-image: url(${landingPageBackgroundImage});
   }
 `
 
-const LogoutLayout = () => {
-  return(
-    <Page>
-      <LogoutNavBar />
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/sign-in" component={SignInPage} />
-        <Route exact path="/sign-up" component={SignUpPage} />
-        <Redirect to="/" />
-      </Switch>
-    </Page>
-  )
-}
+const LogoutLayout = () => (
+  <Page>
+    <LogoutNavBar />
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/sign-in" component={SignInPage} />
+      <Route exact path="/sign-up" component={SignUpPage} />
+      <Redirect to="/" />
+    </Switch>
+  </Page>
+)
+
 
 
 LogoutLayout.propTypes = {
