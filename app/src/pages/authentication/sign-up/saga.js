d3fc51537a435d8279c34a0ca188ce650d9290a0
joris carol <joriscarol@MacBook-Pro-de-joris.local>
2018-07-31 20:15:15 +0200

    Need a fix in sugnup sagga.js (L. 19-24)

--- app/src/pages/authentication/sign-up/saga.js
+++ app/src/pages/authentication/sign-up/saga.js
@@ -1,13 +1,31 @@
-import { takeLatest } from 'redux-saga/effects'
-
-import { SIGNUP_FORM_SUBMIT } from './constants'
+import { call, put, takeLatest } from 'redux-saga/effects'
+import { history, httpClient } from '../../../config'
+import { setToken } from '../../../utils'
+import { SIGNUP_FORM_ERROR, SIGNUP_FORM_SUBMIT } from './constants'
 
 export function* signUpFormSubmit() {
   return yield takeLatest(
     [SIGNUP_FORM_SUBMIT],
     function* ({ payload }) {
-      console.log(payload.values)
-      yield payload.resolve()
+      try {
+        const { data } = yield call(httpClient, {
+          method: 'POST',
+          url: '/authentication/sign-up',
+          data: { ...payload.values}
+        })
+        yield call(setToken, data.token)
+        yield call(history.push, '/dashboard')
+      } catch (error) {
+        document.getElementsByName('username')[0].blur()
+        document.getElementsByName('birthDate')[0].blur()
+        document.getElementsByName('sexualOrientation')[0].blur()
+        document.getElementsByName('gender')[0].blur()
+        document.getElementsByName('email')[0].blur()
+        document.getElementsByName('password')[0].blur()
+        return yield put ({ type: SIGNUP_FORM_ERROR, payload: { error: error.response.data.message } })
+      }
+
+      return yield payload.resolve()
     }
   )
 }
