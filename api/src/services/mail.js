ee382c46d264062a462665f206975ba8b78658a5
joris carol <joriscarol@MacBook-Pro-de-joris.local>
2018-08-09 21:29:42 +0200

    Almost working

--- api/src/services/mail.js
+++ api/src/services/mail.js
@@ -5,15 +5,14 @@ const logger = require('./logger')
 
 const sendEmailConfirm = (userData, token) => {
   sgMail.setApiKey(config.SENDGRID_API_KEY)
-  // sgMail.setSubstitutionWrappers('-!!', '!!-')
+  sgMail.setSubstitutionWrappers('{{', '}}')
   
   const msg = {
     to: userData.email,
     from: 'staff@matcha.com',
     subject: 'Confirm your email',
     templateId: config.EMAIL_TEMPLATES['FR'].CONFIRM_EMAIL_ID,
-    substitutionWrappers: ['-!!', '!!-'],
-    substitutions: {
+    dynamicTemplateData: {
       firstName: userData.firstName,
       link: `${config.APP_END_POINT}/confirm-email?token=${token}`
     }
