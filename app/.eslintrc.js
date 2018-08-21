6aa8c46674c0fb9b013c4c6834fe59a63e2eaffd
mi-mazouz <mickaelmazouz@gmail.com>
2018-08-20 17:35:54 -0700

    prettier config

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,48 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
     },
-    "extends": "eslint:recommended",
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-        "react/jsx-indent": ["error", 2],
-        "react/jsx-indent-props": ["error", 2],
-        "react/jsx-closing-bracket-location": ["error", "tag-aligned"],
-        "react/jsx-tag-spacing": "error",
-        "react/require-default-props": "error",
-        "react/prop-types": "error",
-        "react/jsx-no-undef": "error",
-        "no-multiple-empty-lines": ["error", { "max": 1 }],
-        "comma-dangle": ["error", "never"],
-        "indent": [
-            "error",
-            2,
-            { "SwitchCase": 1 }
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
+    'react/jsx-tag-spacing': 'error',
+    'react/require-default-props': 'error',
+    'react/prop-types': 'error',
+    'react/jsx-no-undef': 'error',
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'comma-dangle': ['error', 'never'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  }
 }
