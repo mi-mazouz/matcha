1aabfa506540926526cbc0e5812d59f73acce304
mi-mazouz <mickaelmazouz@gmail.com>
2018-08-20 17:58:17 -0700

    add prettier for api folder

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,28 +1,15 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "no-multiple-empty-lines": ["error", { "max": 1 }],
-        "comma-dangle": ["error", "never"],
-        "indent": [
-            "error",
-            2,
-            { "MemberExpression": 0, "SwitchCase": 1 }
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  rules: {
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'comma-dangle': ['error', 'never'],
+    indent: ['error', 2, { MemberExpression: 0, SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
+  }
+}
