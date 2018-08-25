fe6e32a737ca39d00b2e89230d0f63661f6145c9
mi-mazouz <mickaelmazouz@gmail.com>
2018-08-25 13:15:49 -0700

    react loadable for every pages

--- app/src/pages/landing/index.js
+++ app/src/pages/landing/index.js
@@ -1,1 +1,10 @@
-export { default } from './Page'
+import Loadable from 'react-loadable'
+
+import Spinner from '../../global/components/Spinner'
+
+const LoadableComponent = Loadable({
+  loader: () => import('./Page'),
+  loading: Spinner
+})
+
+export default LoadableComponent
