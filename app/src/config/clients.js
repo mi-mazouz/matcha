72aca848f9ebe71ebfe8e3c821c1a95d7a37e3b8
mi-mazouz <mickaelmazouz@gmail.com>
2018-09-08 13:16:46 -0700

    fix typo

--- app/src/config/clients.js
+++ app/src/config/clients.js
@@ -1,60 +1,60 @@
-import axios from "axios";
-import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from "apollo-boost";
-import { onError } from "apollo-link-error";
+import axios from 'axios'
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from 'apollo-boost'
+import { onError } from 'apollo-link-error'
 
-import { getToken } from "../utils";
-import getErrorTranslateKey from "./errors";
-import { config } from "../config";
+import { getToken } from '../utils'
+import getErrorTranslateKey from './errors'
+import { config } from '../config'
 
 const authLink = new ApolloLink((operation, forward) => {
-  const token = getToken();
+  const token = getToken()
 
   if (token) {
     operation.setContext({
-      headers: { authorization: "Bearer " + token }
-    });
+      headers: { authorization: 'Bearer ' + token }
+    })
   }
 
-  return forward(operation);
-});
+  return forward(operation)
+})
 
 const afterLink = onError(({ networkError }) => {
   if (networkError && networkError.result) {
     if (networkError.result.errors && networkError.result.errors.length > 0) {
-      networkError.result.message = getErrorTranslateKey(networkError.result.errors[0].message);
+      networkError.result.message = getErrorTranslateKey(networkError.result.errors[0].message)
     } else if (networkError.result.message) {
-      networkError.result.message = getErrorTranslateKey(networkError.result.message);
+      networkError.result.message = getErrorTranslateKey(networkError.result.message)
     }
   }
-});
+})
 
 export const graphqlClient = new ApolloClient({
   link: ApolloLink.from([authLink, afterLink, new HttpLink({ uri: config.GRAPHQL_API_BASE_URI })]),
   cache: new InMemoryCache()
-});
+})
 
 const initInterceptorRequest = client => {
   client.interceptors.request.use(config => {
-    const token = getToken();
+    const token = getToken()
 
     if (token) {
-      config.headers["authorization"] = "Bearer " + token;
+      config.headers['authorization'] = 'Bearer ' + token
     }
 
-    return config;
-  });
+    return config
+  })
 
   client.interceptors.response.use(null, error => {
     if (error.response && error.response.data && error.response.data.message) {
-      error.response.data.message = getErrorTranslateKey(error.response.data.message);
+      error.response.data.message = getErrorTranslateKey(error.response.data.message)
     }
 
-    return Promise.reject(error);
-  });
+    return Promise.reject(error)
+  })
 
-  return client;
-};
+  return client
+}
 
 export const httpClient = initInterceptorRequest(
   axios.create({ baseURL: config.HTTP_API_BASE_URI })
-);
+)
