45a0526a02c71e0fe677d1d7ecaef9fc71dda43e
mi-mazouz <mickaelmazouz@gmail.com>
2018-10-04 17:00:48 +0200

    add production API URI

--- app/config.production.js
+++ app/config.production.js
@@ -1,4 +1,4 @@
 export default {
-  HTTP_API_BASE_URI: 'http://localhost:8080',
-  GRAPHQL_API_BASE_URI: 'http://localhost:8080/graphql'
+  HTTP_API_BASE_URI: 'ec2-35-180-156-7.eu-west-3.compute.amazonaws.com:8080',
+  GRAPHQL_API_BASE_URI: 'ec2-35-180-156-7.eu-west-3.compute.amazonaws.com:8080/graphql'
 }
