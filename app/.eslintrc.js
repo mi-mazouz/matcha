2a9e5a7a9faf712482eebac80e249cbd58950ac1
mi-mazouz <mickaelmazouz@gmail.com>
2019-01-24 12:46:23 +0100

    setup cypress

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -3,35 +3,36 @@ module.exports = {
     browser: true,
     node: true,
     es6: true,
-    jest: true
+    jest: true,
+    'cypress/globals': true
   },
-  extends: ["eslint:recommended", "prettier"],
-  parser: "babel-eslint",
+  extends: ['eslint:recommended', 'prettier'],
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
       jsx: true
     },
-    sourceType: "module"
+    sourceType: 'module'
   },
-  plugins: ["react"],
+  plugins: ['react', 'cypress'],
   rules: {
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "react/jsx-indent": ["error", 2],
-    "react/jsx-indent-props": ["error", 2],
-    "react/jsx-closing-bracket-location": ["error", "tag-aligned"],
-    "react/jsx-tag-spacing": "error",
-    "react/require-default-props": "error",
-    "react/prop-types": "error",
-    "react/jsx-no-undef": "error",
-    "no-multiple-empty-lines": ["error", { max: 1 }],
-    "no-multi-spaces": "error",
-    "comma-dangle": ["error", "never"],
-    "linebreak-style": ["error", "unix"],
-    "newline-per-chained-call": ["error", { ignoreChainWithDepth: 1 }],
-    indent: ["error", 2, { SwitchCase: 1, MemberExpression: 0 }],
-    quotes: ["error", "single"],
-    semi: ["error", "never"]
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-closing-bracket-location': ['error', 'tag-aligned'],
+    'react/jsx-tag-spacing': 'error',
+    'react/require-default-props': 'error',
+    'react/prop-types': 'error',
+    'react/jsx-no-undef': 'error',
+    'no-multiple-empty-lines': ['error', { max: 1 }],
+    'no-multi-spaces': 'error',
+    'comma-dangle': ['error', 'never'],
+    'linebreak-style': ['error', 'unix'],
+    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 1 }],
+    indent: ['error', 2, { SwitchCase: 1, MemberExpression: 0 }],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never']
   }
 }
